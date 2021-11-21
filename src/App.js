e122a6a20677bafbc42b60033cac17ce3bab4c85
gurjung <gurjung997@gmail.com>
2021-11-21 23:39:38 +0530

    adds infoBox initial setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import "./App.css";
 import { Header } from "./Components/Header";
-
+import { InfoBox } from "./Components/InfoBox";
+import { Map } from "./Components/Map";
 // https://disease.sh/v3/covid-19/countries
 function App() {
   return (
     <div className="app">
-      {/* Header */}
-      {/* Title + dropdown countries */}
       <Header />
       <div className="app__stats">
-        {/* info boxes */}
-        {/* info boxes */}
-        {/* info boxes */}
+        <InfoBox title="Corona Cases" cases="2000" totalCases="1232423423" />
+        <InfoBox title="Recovered Cases" cases="2000" totalCases="1232423423" />
+        <InfoBox title="Deaths Cases" cases="2000" totalCases="1232423423" />
       </div>
-
+      <Map />
       {/* Table */}
       {/* chart */}
 
---
+++ src/Components/InfoBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@mui/material/";
+export const InfoBox = ({ title, cases, totalCases }) => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography color="text.secondary">{title}</Typography>
+        <h2>{cases}</h2>
+        <Typography color="text.secondary">{totalCases}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
