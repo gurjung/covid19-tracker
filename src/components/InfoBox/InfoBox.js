796db6937f586cd6a963b985d7351160123b089c
gurjung <gurjung997@gmail.com>
2022-02-26 16:54:11 +0530

    adds styling to infoBox component

--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@mui/material/";
-
+import "./infoBox.css";
 export const InfoBox = ({ title, cases, totalCases }) => {
   return (
-    <Card>
+    <Card className="infoBox">
       <CardContent>
         <Typography color="text.secondary">{title}</Typography>
-        <h2>{cases}</h2>
-        <Typography color="text.secondary">{totalCases}</Typography>
+        <h2 className="infoBox__cases">{cases}</h2>
+        <Typography className="infoBox__total" color="text.secondary">{totalCases}</Typography>
       </CardContent>
     </Card>
   );
