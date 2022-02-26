ee0736e23d4aab670c537f86aa0748bb94fdd600
gurjung <gurjung997@gmail.com>
2022-02-26 17:27:08 +0530

    refactors texts

--- src/constants/texts.js
+++ src/constants/texts.js
@@ -1,6 +1,6 @@
 export const TEXTS={
     APP_HEADING:'COVID19 TRACKER',
     WORLDWIDE:'Worldwide',
-    WORLDWIDE_NEW_CASES: 'Worldwide new cases',
+    WORLDWIDE_NEW: 'Worldwide new',
     LIVE_CASES:'Live Cases by Country'
 }
