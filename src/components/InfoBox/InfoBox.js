b7b2c6a2c4fe2d757fb0726038850a7495d116a4
gurjung <gurjung997@gmail.com>
2022-02-26 16:58:40 +0530

    adds number formatting

--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@mui/material/";
+import numeral from 'numeral';
 import "./infoBox.css";
 export const InfoBox = ({ title, cases, totalCases }) => {
   return (
     <Card className="infoBox">
       <CardContent>
         <Typography color="text.secondary">{title}</Typography>
-        <h2 className="infoBox__cases">{cases}</h2>
-        <Typography className="infoBox__total" color="text.secondary">{totalCases}</Typography>
+        <h2 className="infoBox__cases">{numeral(cases).format("0.0a")}</h2>
+        <Typography className="infoBox__total" color="text.secondary">{numeral(totalCases).format("0.0a")}</Typography>
       </CardContent>
     </Card>
   );
